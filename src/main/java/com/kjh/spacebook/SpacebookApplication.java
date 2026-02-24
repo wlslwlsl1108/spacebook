9c80ffefa6be3435bc5654c97c709d2180a298be
jinhee kim <lsa68803@naver.com>
2026-02-24 15:09:19 +0900

    chore/init: 프로젝트 초기 세팅 (initialize spring boot project)

---
+++ src/main/java/com/kjh/spacebook/SpacebookApplication.java
@@ -0,0 +1,13 @@
+package com.kjh.spacebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpacebookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kjh/spacebook/SpacebookApplicationTests.java
@@ -0,0 +1,13 @@
+// package com.kjh.spacebook;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class SpacebookApplicationTests {
+
+// 	@Test
+// 	void contextLoads() {
+// 	}
+
+// }
